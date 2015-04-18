8141b4daffcc4ad9952bd88b1c3db22b2f3427e7
Seulgi Kim <dev@seulgi.kim>
2015-04-18 20:19:05 +0900

    [Scala] Duration.

---
+++ Scala/Duration/DurationDouble.scala
@@ -0,0 +1,4 @@
+import scala.concurrent.duration.DurationDouble
+1.2.seconds // scala.concurrent.duration.FiniteDuration = 1200 milliseconds
+(1.0/60).seconds // scala.concurrent.duration.FiniteDuration = 16666667 nanoseconds
+
---
+++ Scala/Duration/DurationInt.scala
@@ -0,0 +1,5 @@
+import scala.concurrent.duration.DurationInt
+3.seconds // scala.concurrent.duration.FiniteDuration = 3 seconds
+1.second // scala.concurrent.duration.FiniteDuration = 1 second
+16.milliseconds // scala.concurrent.duration.FiniteDuration = 16 milliseconds
+
