6d234fd64e604fbc8008946964d84d821accad9d
Seulgi Kim <dev@seulgi.kim>
2015-05-24 16:09:37 +0900

    [TypeScript] Variable length arguments with template.

--- TypeScript/variable_arguments.ts
+++ TypeScript/variable_arguments.ts
@@ -5,3 +5,11 @@ function variable_arguments(...args: number[]): number[] {
 variable_arguments();
 variable_arguments(0);
 variable_arguments(0, 1);
+
+function variable_arguments_with_template<T>(...args: T[]): T[] {
+  return args;
+}
+
+variable_arguments_with_template<number>(1, 2, 3);
+variable_arguments_with_template<string>("a");
+variable_arguments_with_template<boolean[]>([true, false]);
