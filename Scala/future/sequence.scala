d33705afaa9bc6a1c73202559f93a9655c70c7bb
Seulgi Kim <dev@seulgi.kim>
2015-06-24 11:15:14 +0900

    [Scala][future] returns the first failed future.

--- Scala/future/sequence.scala
+++ Scala/future/sequence.scala
@@ -18,3 +18,14 @@ val f9 = Future.successful(Seq(4, 5, 6))
 val f10 = Future.sequence(Seq(f6, f7, f8, f9))
 
 f10.map { case a => println(s"$a") } // List(1, List(2), List(3), List(4, 5, 6))
+
+val f11 = Future.successful(1)
+val f12 = Future {
+  Thread sleep 10000
+  throw new Exception("a")
+}
+val f13 = Future.failed(new Exception("b"))
+
+val f14 = Future.sequence(Seq(f11, f12, f13))
+
+f14.onFailure { case a => println(s"$a") } // java.lang.Exception: a
