a6b6fc5872e875414dfe9124bbc61d38159646aa
Seulgi Kim <dev@seulgi.kim>
2015-07-04 16:05:33 +0900

    [Scala][Future][fallbackTo]

--- Scala/future/fallbackTo.scala
+++ Scala/future/fallbackTo.scala
@@ -17,3 +17,21 @@ val f6 = Future {
 
 val f7 = f3 fallbackTo f6
 f7.onComplete(println) // print Success(100) immediately
+
+
+class A {
+  override def toString: String = "A"
+}
+class B extends A {
+  override def toString: String = "B"
+}
+val f8: Future[A] = Future.successful(new A)
+val f9: Future[B] = Future.successful(new B)
+val f10: Future[A] = Future.failed(new Exception ("a"))
+val f11: Future[B] = Future.failed(new Exception ("b"))
+
+f8 fallbackTo f9 onComplete println
+f9 fallbackTo f8 onComplete println
+
+f10 fallbackTo f8 onComplete println
+f11 fallbackTo f9 onComplete println
