c5d9a7eb448946e86409f20f84504594eabca605
Seulgi Kim <dev@seulgi.kim>
2015-08-11 15:41:12 +0900

    [ECMAScript 6] class

---
+++ javascript/class.js
@@ -0,0 +1,39 @@
+class Base {
+  static static_method1() {
+    return console.log('base static method 1');
+  }
+
+  static static_method2() {
+    return console.log('base static method 2');
+  }
+
+  prototype_method1() {
+    return console.log('base prototype method 1');
+  }
+
+  prototype_method2() {
+    return console.log('base prototype method 2');
+  }
+}
+
+class Derived extends Base {
+  static static_method1() {
+    return console.log('derived static method 1');
+  }
+
+  prototype_method1() {
+    return console.log('derived prototype method 1');
+  }
+}
+
+Base.static_method1(); // base static method 1
+Base.static_method2(); // base static method 2
+let b = new Base();
+b.prototype_method1(); // base prototype method 1
+b.prototype_method2(); // base prototype method 2
+
+Derived.static_method1(); // derived static method 1
+Derived.static_method2(); // base static method 2
+let d = new Derived();
+d.prototype_method1(); // derived prototype method 1
+d.prototype_method2(); // base prototype method 2
