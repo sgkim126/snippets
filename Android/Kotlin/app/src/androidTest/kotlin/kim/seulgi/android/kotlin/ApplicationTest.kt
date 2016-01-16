dcd464ef4904e9f8e2beab57ca81f543d2355c74
Seulgi Kim <sgkim126@gmail.com>
2016-01-16 12:14:02 +0900

    [Android][Kotlin] Convert to Kotlin
    ```ctrl + shift + a -> Convert Java File to Kotlin File```
    or
    ```ctrl + shift + alt + k````

---
+++ Android/Kotlin/app/src/androidTest/kotlin/kim/seulgi/android/kotlin/ApplicationTest.kt
@@ -0,0 +1,9 @@
+package kim.seulgi.android.kotlin
+
+import android.app.Application
+import android.test.ApplicationTestCase
+
+/**
+ * [Testing Fundamentals](http://d.android.com/tools/testing/testing_android.html)
+ */
+class ApplicationTest : ApplicationTestCase<Application>(Application::class.java)
