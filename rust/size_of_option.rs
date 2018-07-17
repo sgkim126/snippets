c9e3847d9f0b16f3fc68daef5faa8cf4e93ddfc3
Seulgi Kim <seulgi.kim@kodebox.io>
2018-07-17 11:20:01 +0900

    [Rust] The flag for Option is aligned

--- rust/size_of_option.rs
+++ rust/size_of_option.rs
@@ -1,10 +1,21 @@
 use std::mem::size_of;
 
+#[allow(dead_code)]
+struct S {
+    a: u64,
+    b: u64,
+    c: u64,
+    d: u64,
+}
+
 fn main() {
     println!("{}", size_of::<u32>()); // 4
     println!("{}", size_of::<u64>()); // 8
     println!("{}", size_of::<Option<u32>>()); // 8
     println!("{}", size_of::<Option<u64>>()); // 16
 
-    assert_eq!(size_of::<u32>() * 2, size_of::<u64>());
+    println!("{}", size_of::<S>()); // 32
+    println!("{}", size_of::<Option<S>>()); // 40
+
+    assert_eq!(size_of::<S>() + 8, size_of::<Option<S>>());
 }
