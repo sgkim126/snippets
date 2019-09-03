66cd1641038edf6353d27eddc5e03bb14c222c33
Seulgi Kim <dev@seulgi.kim>
2019-09-03 22:34:23 +0900

    [Chrome Extension] Layer Logic
    https://developer.chrome.com/extensions/getstarted#logic

--- chrome-extension/getstarted/popup.js
+++ chrome-extension/getstarted/popup.js
@@ -6,3 +6,12 @@ chrome.storage.sync.get('color', function(data) {
   changeColor.style.backgroundColor = data.color;
   changeColor.setAttribute('value', data.color);
 });
+
+changeColor.onclick = function(element) {
+  const color = element.target.value;
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.executeScript(
+      tabs[0].id,
+      {code: 'document.body.style.backgroundColor = "' + color + '";'});
+  });
+};
