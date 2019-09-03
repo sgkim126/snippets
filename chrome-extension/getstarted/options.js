bff1f9a0d757bae49717fb2597ec1a24a7dae1bc
Seulgi Kim <dev@seulgi.kim>
2019-09-03 22:56:04 +0900

    [Chrome Extension] Give User Options
    https://developer.chrome.com/extensions/getstarted#options

---
+++ chrome-extension/getstarted/options.js
@@ -0,0 +1,15 @@
+const page = document.getElementById('buttonDiv');
+const kButtonColors = ['#3aa757', '#e8453c', '#f9bb2d', '#4688f1'];
+function constructOptions(kButtonColors) {
+  for (const item of kButtonColors) {
+    const button = document.createElement('button');
+    button.style.backgroundColor = item;
+    button.addEventListener('click', function() {
+      chrome.storage.sync.set({color: item}, function() {
+        console.log('color is ' + item);
+      })
+    });
+    page.appendChild(button);
+  }
+}
+constructOptions(kButtonColors);
