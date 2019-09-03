d19e3d60674af139a8ff29c8198ee03bbaeb29eb
Seulgi Kim <dev@seulgi.kim>
2019-09-03 22:02:37 +0900

    [Chrome Extension] Introduce a User Interface
    https://developer.chrome.com/extensions/getstarted#user_interface

---
+++ chrome-extension/getstarted/popup.js
@@ -0,0 +1,8 @@
+"use strict";
+
+const changeColor = document.getElementById('changeColor');
+
+chrome.storage.sync.get('color', function(data) {
+  changeColor.style.backgroundColor = data.color;
+  changeColor.setAttribute('value', data.color);
+});
