0f90c5770f93562d6c07022647251fc70ed31cc7
Seulgi Kim <dev@seulgi.kim>
2025-11-16 23:23:26 +0900

    [Flutter][3d] use ThreeJsWidget

--- flutter/threed/lib/main.dart
+++ flutter/threed/lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:threed/screens/flutter_3d_controller_widget.dart';
+import 'package:threed/screens/three_js_widget.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
 
@@ -11,12 +13,58 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter 3D',
+      title: 'Flutter 3D Demos',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Flutter3DControllerWidget(title: 'Flutter 3D Controller'),
+      home: const MainScreen(),
+    );
+  }
+}
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _selectedIndex = 0;
+
+  static const List<Widget> _widgetOptions = <Widget>[
+    ThreeJsWidget(title: 'three_js'),
+    Flutter3DControllerWidget(title: 'flutter_3d_controller'),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: IndexedStack(
+        index: _selectedIndex,
+        children: _widgetOptions,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: SizedBox.shrink(),
+            label: 'Three.js',
+          ),
+          BottomNavigationBarItem(
+            icon: SizedBox.shrink(),
+            label: '3D Controller',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+      ),
     );
   }
 }
